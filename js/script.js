dbcb8c70526f0d90e946c503519282365a0fb4f4
Nhut Nguyen <nhutnguyen@vba-adk.com>
2025-07-14 16:40:55 +0700

    up

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(window).load(function(){
     AOS.init({
         once: false,
         offset: 280, // offset (in px) from the original trigger point
-        delay: 300, // values from 0 to 3000, with step 50ms
+        delay: 1, // values from 0 to 3000, with step 50ms
         //duration:500, // values from 0 to 3000, with step 50ms
         easing: 'ease-in-sine',
         mirror: true,
